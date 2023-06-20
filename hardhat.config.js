ce2f49a6fe57d9a15c1823fcbfbb22791747dbd0
YYovchev123 <102542112+YYovchev123@users.noreply.github.com>
2023-06-20 22:14:19 +0300

    sec-commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,10 +7,8 @@ module.exports = {
   networks: {
     hardhat: {},
     sepolia: {
-      url: `https://sepolia.infura.io/v3/fb81ff63ff0a41f191d1c3d5b4a2058f`,
-      accounts: [
-        "2b0a0ad3ad74930da8dda26e38185b2938781c2d9cd25de51a8e71d521b74f8a",
-      ],
+      url: ``,
+      accounts: [""],
     },
   },
 };
